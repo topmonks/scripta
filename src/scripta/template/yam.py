a59d9bddcc17c08ad9f0346ccef293a85b6de783
Robert Vojta <rvojta@me.com>
2017-05-24 00:43:12 +0200

    keep dict items order

--- src/scripta/template/yam.py
+++ src/scripta/template/yam.py
@@ -1,9 +1,21 @@
 
 import yaml
+import collections
 from copy import deepcopy
 from functools import reduce
 from itertools import chain, starmap
 
+
+_mapping_tag = yaml.resolver.BaseResolver.DEFAULT_MAPPING_TAG
+
+def dict_representer(dumper, data):
+    return dumper.represent_dict(data.items())
+
+def dict_constructor(loader, node):
+    return collections.OrderedDict(loader.construct_pairs(node))
+
+yaml.add_representer(collections.OrderedDict, dict_representer)
+yaml.add_constructor(_mapping_tag, dict_constructor)
 
 # YAML functions
 
